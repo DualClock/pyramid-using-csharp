4dd5527bf2c58a4b504e37a315cba683b74ba44e
DualClock <kompasmini@yandex.com>
2025-02-17 23:38:02 +0300

    Add files via upload

---
+++ Peramide/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peramide
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreatePiramide(10);
+        }
+        static void CreatePiramide(int hight)
+        {
+            for (int i = 0; i < hight; i++)
+            {
+                for (int j=0; j < hight - 1 - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < (hight - (hight-1)) + i * 2; k++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+                
+            }
+        }
+    }
+}
